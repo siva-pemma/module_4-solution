38d01cbd3f60d9c2af13d2345f60708fb485907f
Siva pemma <63335567+siva-pemma@users.noreply.github.com>
2020-05-07 16:38:06 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var names=["yakoov","John","Paul","Jin","Alex","Ashok","JIm","joker","Paul","jimin"];
+for(var i=0;i<names.length;i++){
+	var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+	if(firstLetter=='j'){
+		console.log("Goodbye " +names[i]);
+
+	}
+	else{
+		console.log("Heloo " +names[i]);
+	}
+}
